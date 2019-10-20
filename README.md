be02cebcc7086f5e4cf47259486fe3bf3eb467e9
Drew Miley <drewmiley92@gmail.com>
2019-10-21 00:38:34 +0100

    Modifictions to script

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 2. Open in browser
 3. Open dev tools
 4. Copy contents of script.js into dev tools console
-5. Run `refresh()` in console
+5. Set `const textToFind = TEXT_TO_FIND`. For test page, use Math.pow(10, n).
+6. Set `const logTime = BOOLEAN`.
+7. Run `refresh(textToFind, logTime)` in console.
--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const refresh = () => {
-    console.log('script')
+const refresh = (textToFind, logTime = false) => {
+    const start = logTime ? Date.now() : null;
+    let refreshes = 1;
+    location.reload();
+
+    window.onload = function() {
+        refreshes++;
+        if (!document.body.innerText.includes(textToFind)) {
+            location.reload();
+        }
+    }
+
+    const end = logTime ? Date.now() : null;
+    if (logTime) {
+        console.log(`Time taken in seconds: ${ Math.ceil((end - start) / 1000) }`)
+        console.log(`Total refreshes: ${ refreshes }`)
+    }
 }
