5e7f4319e0233fc292e7e71e014794114f4eaa08
Drew Miley <drewmiley92@gmail.com>
2019-10-20 23:55:13 +0100

    Number displays powers of 10

--- random.js
+++ random.js
@@ -1,1 +1,8 @@
-document.getElementById('random').innerText = Math.random();
+let number = Math.random();
+let result = '1';
+while (number > 0.9) {
+    result += '0';
+    number = (number * 10) % 1;
+}
+
+document.getElementById('random').innerText = result;
